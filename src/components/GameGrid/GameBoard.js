e6341569f74abb8eeb23118cb84dde3deb3a9d39
David Wheatley <hi@davwheat.dev>
2021-01-14 20:35:06 +0000

    Refactor KnockoutRounds into another component

--- src/components/GameGrid/GameBoard.js
+++ src/components/GameGrid/GameBoard.js
@@ -1,34 +1,11 @@
 import React from 'react'
 
-import PollGame from './PollGame'
-import { Paragraph, Shout, Whisper } from '../../typography'
-
-import FormatDate from '../../functions/formatDate'
-
-import KnownGameData from '../../data/GameData'
-import { GetTocColor, GetTocName } from '../../data/TocData'
-import { makeStyles } from '@material-ui/styles'
+import { Paragraph, Shout } from '../../typography'
 import TextContainer from '../TextContainer'
 
-const useStyles = makeStyles({
-  knockoutRoundsContainer: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    gap: 8,
-    margin: 'auto',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  gameContainer: {
-    marginTop: 8,
-    width: 'max-content',
-    textAlign: 'center',
-  },
-})
+import KnownGameData from '../../data/GameData'
 
 export default function GameBoard() {
-  const classes = useStyles()
-
   return (
     <>
       <TextContainer>
@@ -36,23 +13,7 @@ export default function GameBoard() {
         <Paragraph>Knockout rounds begin on Friday 15 January and end on Saturday 30 January.</Paragraph>
       </TextContainer>
 
-      <section className={classes.knockoutRoundsContainer}>
-        {KnownGameData.KnockoutRounds.map((round, i) => {
-          const teamInfo = {
-            team1: GetTocName(round.team1),
-            team2: GetTocName(round.team2),
-            team1color: GetTocColor(round.team1),
-            team2color: GetTocColor(round.team2),
-          }
-
-          return (
-            <div className={classes.gameContainer} key={i}>
-              <Whisper bold>{FormatDate(round.date)}</Whisper>
-              <PollGame teamInfo={teamInfo} />
-            </div>
-          )
-        })}
-      </section>
+      <KnockoutRounds />
     </>
   )
 }
---
+++ src/components/GameGrid/Rounds/KnockoutRounds.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+import { GetTocColor, GetTocName } from '../../data/TocData'
+
+import FormatDate from '../../functions/formatDate'
+import { makeStyles } from '@material-ui/styles'
+import { Whisper } from '../../../typography'
+import PollGame from '../PollGame'
+
+const useStyles = makeStyles({
+  knockoutRoundsContainer: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    gap: 8,
+    margin: 'auto',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  gameContainer: {
+    marginTop: 8,
+    width: 'max-content',
+  },
+})
+
+export default function KnockoutRounds() {
+  const classes = useStyles()
+
+  return (
+    <section className={classes.knockoutRoundsContainer}>
+      {KnownGameData.KnockoutRounds.map((round, i) => {
+        const teamInfo = {
+          team1: GetTocName(round.team1),
+          team2: GetTocName(round.team2),
+          team1color: GetTocColor(round.team1),
+          team2color: GetTocColor(round.team2),
+        }
+
+        return (
+          <div className={classes.gameContainer} key={i}>
+            <Whisper center bold>
+              {FormatDate(round.date)}
+            </Whisper>
+            <PollGame teamInfo={teamInfo} />
+          </div>
+        )
+      })}
+    </section>
+  )
+}
