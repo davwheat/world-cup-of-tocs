0f04635e09fb029500cc64ae56741f5e2e9dd081
Kishan <kishansambhi@hotmail.co.uk>
2021-01-15 00:07:41 +0000

    Footer changes

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,9 +11,10 @@ export default function Footer() {
   return (
     <BodySection>
       <Paragraph>
-        Made with <span>❤️</span> by David Wheatley - &copy; {new Date().getFullYear()} All Rights Reserved
+        Made with <span>❤️</span> by David Wheatley & Kishan Singh - &copy; {new Date().getFullYear()} All Rights Reserved
       </Paragraph>
       <Paragraph>
+        <h4>Find David here:</h4>
         <Link target="_blank" url="https://github.com/davwheat">
           GitHub
         </Link>
@@ -30,8 +31,27 @@ export default function Footer() {
           Email me
         </Link>
 
+        
+      </Paragraph>
+      <Paragraph>
+        <h4>Find Kishan here:</h4>
+        <Link target="_blank" url="https://github.com/Gum-Joe">
+          GitHub
+        </Link>
+
         <BulletSeparator />
 
+        <Link target="_blank" url="https://twitter.com/official_gumjoe">
+          Twitter
+        </Link>
+
+        <BulletSeparator />
+
+        <Link target="_blank" url="mailto:jaskishansaran@gmail.com">
+          Email me
+        </Link>
+      </Paragraph>
+      <Paragraph>
         <Link onClick={() => window.cookiehub.openSettings()}>Cookie settings</Link>
       </Paragraph>
       <Whisper>
