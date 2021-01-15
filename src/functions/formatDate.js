e281672c0e20bb12d6cade76783200474bdcfac3
David Wheatley <hi@davwheat.dev>
2021-01-15 09:32:19 +0000

    Fix incorrect date formatting

--- src/functions/formatDate.js
+++ src/functions/formatDate.js
@@ -1,5 +1,5 @@
 import dayjs from 'dayjs'
 
 export default function FormatDate(unixTimestamp) {
-  return dayjs(new Date(unixTimestamp)).format('ddd D MMM')
+  return dayjs(new Date(unixTimestamp)).utc().format('ddd D MMM')
 }
