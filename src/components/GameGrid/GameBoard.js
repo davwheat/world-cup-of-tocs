3b84da0233bfe2ccdd9eb5439facc5b721c6e36e
David Wheatley <hi@davwheat.dev>
2021-01-15 22:57:21 +0000

    Add warning about graph treating data as discrete.

--- src/components/GameGrid/GameBoard.js
+++ src/components/GameGrid/GameBoard.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Paragraph, Shout } from '../../typography'
+import { Paragraph, Shout, Whisper } from '../../typography'
 import TextContainer from '../TextContainer'
 
 import KnockoutRounds from './Rounds/KnockoutRounds'
@@ -17,7 +17,11 @@ export default function GameBoard(props) {
     <>
       <TextContainer>
         <Shout>Knockout rounds</Shout>
-        <Paragraph>Knockout rounds begin on Friday 15 January and end on Saturday 30 January.</Paragraph>
+        <Paragraph>Knockout rounds begin on Friday 15 January and end on Saturday 30&nbsp;January.</Paragraph>
+        <Whisper>
+          The graphs aren't representative of the true time period between each data point, and we're working on fixing that at the moment. Take rapid increases
+          and decreases with a grain of&nbsp;salt.
+        </Whisper>
       </TextContainer>
 
       <KnockoutRounds knockoutRoundData={gameData.knockout} />
