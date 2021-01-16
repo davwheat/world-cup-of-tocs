68c3f3a692683d6f55ffd9ad661d09cc84e36525
David Wheatley <hi@davwheat.dev>
2021-01-16 04:44:52 +0000

    Add "ending in x xx" text

--- src/components/GameGrid/Rounds/GameRound.tsx
+++ src/components/GameGrid/Rounds/GameRound.tsx
@@ -2,16 +2,18 @@ import React from 'react'
 
 import { SinglePoll, default as Graph } from '../Graph'
 import PollGame from '../PollGame'
-import { Whisper } from '../../../typography'
+import { Paragraph, Whisper } from '../../../typography'
 
 import { makeStyles } from '@material-ui/styles'
 
 import { GetTocColor, GetTocName } from '../../../data/TocData'
 import FormatDate from '../../../functions/formatDate'
+import clsx from 'clsx'
 
 interface Props {
   data: SinglePoll
   noDate: boolean
+  large: boolean
 }
 
 const useStyles = makeStyles({
@@ -28,38 +30,49 @@ const useStyles = makeStyles({
     width: 500,
     maxWidth: '100%',
   },
+  large: {
+    display: 'flex',
+    width: '100%',
+  },
 })
 
-export default function GameRound(props) {
-  const { data: gamePoll, noDate } = props
+export default function GameRound(props: Props) {
+  const { data, noDate, large } = props
   const classes = useStyles()
 
   const teamInfo = {
-    team1: GetTocName(gamePoll.votesInfo[0].tocReportingMark),
-    team2: GetTocName(gamePoll.votesInfo[1].tocReportingMark),
-    team1color: GetTocColor(gamePoll.votesInfo[0].tocReportingMark),
-    team2color: GetTocColor(gamePoll.votesInfo[1].tocReportingMark),
+    team1: GetTocName(data.votesInfo[0].tocReportingMark),
+    team2: GetTocName(data.votesInfo[1].tocReportingMark),
+    team1color: GetTocColor(data.votesInfo[0].tocReportingMark),
+    team2color: GetTocColor(data.votesInfo[1].tocReportingMark),
   }
 
   return (
-    <div className={classes.gameContainer}>
+    <div className={clsx(classes.gameContainer, large && classes.large)}>
       {!noDate && (
         <Whisper center bold>
-          {FormatDate(gamePoll.scheduledStartDay)}
+          {FormatDate(data.scheduledStartDay)}
         </Whisper>
       )}
-      <PollGame
-        teamInfo={teamInfo}
-        hasStarted={gamePoll.votingStatus !== 'UPCOMING'}
-        voteInfo={{
-          votes1: gamePoll.votesInfo[0].votes,
-          votes2: gamePoll.votesInfo[1].votes,
-        }}
-        tweetId={gamePoll.twitterInfo && gamePoll.twitterInfo.tweetId}
-      />
+      <div>
+        {large && (
+          <Paragraph bold center>
+            Ending in {FormatDate.HoursMins.Long(new Date(data.twitterInfo.endTime).getTime() - new Date().getTime())}
+          </Paragraph>
+        )}
+        <PollGame
+          teamInfo={teamInfo}
+          hasStarted={data.votingStatus !== 'UPCOMING'}
+          voteInfo={{
+            votes1: data.votesInfo[0].votes,
+            votes2: data.votesInfo[1].votes,
+          }}
+          tweetId={data.twitterInfo && data.twitterInfo.tweetId}
+        />
+      </div>
 
       {/* Don't try to SSR graph */}
-      {typeof window !== 'undefined' && <Graph poll={gamePoll} />}
+      {typeof window !== 'undefined' && <Graph poll={data} />}
     </div>
   )
 }
