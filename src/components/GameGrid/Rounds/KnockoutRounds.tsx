168cd8fd923e4fc3a5f695ca9af739cae2379bd3
David Wheatley <hi@davwheat.dev>
2021-01-16 14:33:26 +0000

    Move active game panel to top of page and use new component to render it

--- src/components/GameGrid/Rounds/KnockoutRounds.tsx
+++ src/components/GameGrid/Rounds/KnockoutRounds.tsx
@@ -4,8 +4,6 @@ import GameRound from './GameRound'
 
 import { makeStyles } from '@material-ui/styles'
 
-import TextContainer from '../../TextContainer'
-import Loud from '../../../typography/loud'
 import SinglePoll from '../../../models/SinglePoll'
 
 const useStyles = makeStyles({
@@ -22,15 +20,6 @@ const useStyles = makeStyles({
     width: 500,
     maxWidth: '100%',
   },
-  latestGame: {
-    padding: 16,
-    border: '2px solid #000',
-  },
-  latestGameContainer: {
-    marginTop: -16,
-    display: 'flex',
-    justifyContent: 'center',
-  },
 })
 
 type KnockoutRoundData = Record<number, SinglePoll>
@@ -44,25 +33,8 @@ const KnockoutRounds: React.FC<Props> = ({ knockoutRoundData }) => {
 
   if (!knockoutRoundData) return null
 
-  const latestGame =
-    knockoutRoundData[
-      Object.keys(knockoutRoundData)
-        .sort((a, b) => Number(b) - Number(a))
-        .find(k => knockoutRoundData[k].votingStatus === 'IN_PROGRESS')
-    ]
-
   return (
     <>
-      {latestGame && (
-        <TextContainer innerClassName={classes.latestGame}>
-          <Loud center>Active game</Loud>
-          <div className={classes.latestGameContainer}>
-            <GameRound large noDate data={latestGame} />
-          </div>
-        </TextContainer>
-      )}
-
-      {latestGame && <Loud center>Other games</Loud>}
       <section className={classes.knockoutRoundsContainer}>
         {Object.keys(knockoutRoundData).map(key => (
           <GameRound data={knockoutRoundData[key]} key={key} />
