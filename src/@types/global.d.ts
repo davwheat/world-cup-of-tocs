151a79f940ea8635fee775a0ae8a869bcde576bc
David Wheatley <hi@davwheat.dev>
2021-01-17 16:05:34 +0000

    Merge typing files

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -1,5 +1,5 @@
-// Trick TS into letting us modify the global scope
-export {}
+import { TOCCode } from '../data/TocData'
+import SinglePoll from '../models/SinglePoll'
 
 declare global {
   // We need this to add more properties to `window` so TS doesn't complain
@@ -10,5 +10,86 @@ declare global {
        */
       openSettings: () => void
     }
+  }
+
+  export enum VoteStates {
+    UPCOMING = 'UPCOMING',
+    IN_PROGRESS = 'IN_PROGRESS',
+    DONE = 'DONE',
+  }
+
+  export interface IAPIResponse {
+    apiVersion: string
+    knockout: Record<number, ISinglePoll>
+    groupStages: Record<number, ISinglePoll>
+    quarterFinal: Record<number, ISinglePoll>
+    semiFinal: Record<number, ISinglePoll>
+    runnerUp: ISinglePoll
+    final: ISinglePoll
+  }
+
+  export interface IGameData {
+    knockout: Record<number, SinglePoll>
+    groupStages: Record<number, SinglePoll>
+    quarterFinal: Record<number, SinglePoll>
+    semiFinal: Record<number, SinglePoll>
+    runnerUp: SinglePoll
+    final: SinglePoll
+  }
+
+  /**
+   * Defines how a single poll should look
+   */
+  export interface ISinglePoll {
+    /**
+     * Timestamp of midnight UTC on the day of the poll */
+    scheduledStartDay: number
+
+    votingStatus: VoteStates
+
+    twitterInfo?: ITwitterInfo
+
+    /**
+     * Actual votes for each TOC
+     */
+    votesInfo: IVotesInfo[]
+  }
+
+  export interface ITwitterInfo {
+    /**
+     * Tweet ID (not URL)
+     */
+    tweetId: string
+    /**
+     * Unix timestamp of poll start
+     */
+    startTime: number
+    /**
+     * Unix timestamp where poll will or did end
+     */
+    endTime: number
+    /**
+     * Duration of poll in minutes
+     */
+    durationMinutes: number
+  }
+  export interface IVotesInfo {
+    /**
+     * Two letter TOC reporting mark
+     */
+    tocReportingMark: TOCCode
+    votes: number
+    /**
+     * Voting history
+     */
+    votingHistory: IVotingHistoryEntry[]
+  }
+
+  export interface IVotingHistoryEntry {
+    /**
+     * UNIX timestamp of time when this vote count was active
+     */
+    timestamp: number
+    votes: number
   }
 }
