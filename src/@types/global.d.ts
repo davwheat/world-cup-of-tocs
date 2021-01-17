5d8773a72e12e12162d49ddc2315c842b17941e7
David Wheatley <hi@davwheat.dev>
2021-01-17 14:21:21 +0000

    Add comments to window interface

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -1,8 +1,13 @@
+// Trick TS into letting us modify the global scope
 export {}
 
 declare global {
+  // We need this to add more properties to `window` so TS doesn't complain
   interface Window {
     cookiehub: {
+      /**
+       * Opens the CookieHub cookie settings window where users can change their cookie preferences.
+       */
       openSettings: () => void
     }
   }
