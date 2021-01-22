a8697a6546fe07b9cd105ec6491a790c2f7bc415
David Wheatley <hi@davwheat.dev>
2021-01-22 11:26:56 +0000

    Handle 0 and null timestamps
    Prerequisite of: https://github.com/davwheat/world-cup-of-tocs-api/pull/2/

--- src/functions/formatDate.ts
+++ src/functions/formatDate.ts
@@ -1,14 +1,35 @@
 import dayjs from 'dayjs'
 
 const FormatDate = (unixTimestamp: number): string => {
+  if (unixTimestamp === 0) {
+    return 'Date unknown'
+  }
+  if (unixTimestamp === null) {
+    return 'Date TBC'
+  }
+
   return dayjs(unixTimestamp).utc().format('ddd D MMM')
 }
 
 FormatDate.HoursMins = (unixTimestamp: number): string => {
+  if (unixTimestamp === 0) {
+    return 'Date unknown'
+  }
+  if (unixTimestamp === null) {
+    return 'Date TBC'
+  }
+
   return dayjs(unixTimestamp).utc().format('H:mm')
 }
 
 FormatDate.HoursMinsLong = (unixTimestamp: number): string => {
+  if (unixTimestamp === 0) {
+    return 'Date unknown'
+  }
+  if (unixTimestamp === null) {
+    return 'Date TBC'
+  }
+
   return dayjs(unixTimestamp).utc().format('H[h] mm[m]')
 }
 
