403e31613b8644b8e7f6e3a03ed473176b2d48a7
David Wheatley <hi@davwheat.dev>
2021-01-23 17:31:54 +0000

    Fix modal offset on mobile

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -26,6 +26,11 @@ const useStyles = makeStyles({
     [`@media ${Breakpoints.upTo.medium}`]: {
       height: '100vh',
       maxHeight: 'none',
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+      transform: 'none',
     },
   },
   backdrop: {
