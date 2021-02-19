f5b87ceec858fd8a69295bfc1b4ca238e11e6a7e
Kishan <kishansambhi@hotmail.co.uk>
2021-02-19 12:05:07 +0000

    Add Table component

---
+++ src/components/Table.tsx
@@ -0,0 +1,18 @@
+/**
+ * Rank table for rankings
+ */
+import React from 'react'
+
+interface Props {
+  a?: string
+}
+
+const Table: React.FC<Props> = () => {
+  return (
+    <table>
+      <h1>MEAP</h1>
+    </table>
+  )
+}
+
+export default Table
